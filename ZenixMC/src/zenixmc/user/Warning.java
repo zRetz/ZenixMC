842d87e8fb8bdd6f48a79cd5ec9f841f049200ac
jaymsDooku <james.leaver2@btinternet.com>
2015-07-21 02:16:33 +0100

    completed warning class

--- ZenixMC/src/zenixmc/user/Warning.java
+++ ZenixMC/src/zenixmc/user/Warning.java
@@ -16,35 +16,74 @@ public class Warning {
     /**
      * The map of amount of warnings and punishment timestamp.
      */
-    private TreeMap<Integer, Long> values;
+    private final TreeMap<Integer, Long> values;
     
+    /**
+     * Instantiates a brand new warning.
+     */
     public Warning() {
         this.values = new TreeMap<>();
         put(0, 0L);
     }
     
+    /**
+     * Instantiates a warning with set values.
+     * @param amount The amount of warnings.
+     * @param timestamp The punishment stamp.
+     */
     public Warning(int amount, long timestamp) {
         this.values = new TreeMap<>();
         put(amount, timestamp);
     }
     
-    public void increment(long timestamp) {
+    /**
+     * Adds a warning and the punishment timestamp.
+     * @param timestamp The punishment timestamp.
+     * @return If it successfully incremented.
+     */
+    public boolean increment(long timestamp) {
         
         int amount = values.firstKey();
         
         //temp const
-        if (amount > 3) {
-            return;
+        if (amount >= 3) {
+            return false;
         }
         
-        long currentTimestamp = values.get(amount);
-        put(amount + 1, currentTimestamp + timestamp);
+        put(amount + 1, values.get(amount) + timestamp);
+        return true;
     }
     
-    public void decrement() {
+    /**
+     * Subtracts a warning but keeps the punishment timestamp as it is.
+     * @return If it successfully decremented.
+     */
+    public boolean decrement() {
         
+        int amount = values.firstKey();
+        int nAmount = amount - 1;
+        
+        if (nAmount < 0) {
+            return false;
+        }
+        
+        put(nAmount, values.get(amount));
+        return true;
     }
     
+    /**
+     * @return If amount has reached maximum warnings.
+     */
+    public boolean isMaximum() {
+        //temp const
+        return values.firstKey() >= 3;
+    }
+    
+    /**
+     * Puts values into the values map.
+     * @param amount The amount of warnings (key).
+     * @param timestamp The punishment timestamp (value).
+     */
     private void put(int amount, long timestamp) {
         values.clear();
         values.put(amount, timestamp);
