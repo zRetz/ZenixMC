1321422f13244ec64df8254def93305f5a6acabf
jaymsDooku <james.leaver2@btinternet.com>
2015-07-21 03:41:21 +0100

    some more backend work done

---
+++ ZenixMC/src/zenixmc/user/objects/Home.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zenixmc.user.objects;
+
+import org.bukkit.Location;
+import zenixmc.user.ZenixUserInterface;
+
+/**
+ * Personal set location.
+ * @author james
+ */
+public class Home {
+    
+    /**
+     * The owner of the home.
+     */
+    private ZenixUserInterface zui;
+    
+    /**
+     * The name of the home.
+     */
+    private final String name;
+    
+    /**
+     * The location of the home.
+     */
+    private final Location location;
+    
+    /**
+     * Instantiates a home with set values.
+     * @param name
+     *      The name of the home.
+     * @param location
+     *      The location of the home.
+     */
+    public Home(String name, Location location) {
+        this.name = name;
+        this.location = location;
+    }
+    
+    public void goTo() {
+        
+    }
+    
+    /**
+     * @return The owner of the home.
+     */
+    public ZenixUserInterface getZui() {
+        return zui;
+    }
+    
+    /**
+     * @return The name of the home.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return The location of the home.
+     */
+    public Location getLocation() {
+        return location;
+    }
+}
