a7843910e32bc66a55e4815e5e56d38515ae6ed3
BuildTools <unconfigured@null.spigotmc.org>
2015-07-27 14:42:42 -0400

    Fixed bugs

--- src/zenixmc/organization/Clan.java
+++ src/zenixmc/organization/Clan.java
@@ -1,138 +1,209 @@
-package zenixmc.command.commands.clans;
+package zenixmc.organization;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import zenixmc.ZenixMCInterface;
-import zenixmc.command.ZenixCommandSender;
-import zenixmc.organization.Clan;
-import zenixmc.organization.Message;
+import org.bukkit.Location;
+
 import zenixmc.organization.Message.Type;
+import zenixmc.user.ZenixUserInterface;
 
-public class ClanCommands extends AbstractClanCommand {
-	protected int commandsPerPage = 10;
-	public String name;
+public class Clan implements Organization {
+	public static List<String> Clans = new ArrayList<>();
+	public static HashMap<ZenixUserInterface, String> playerClan = new HashMap<>();
+	public static HashMap<ZenixUserInterface, String> clanInvites;
+	public static HashMap<Location, String> clanHomes;
 
-	public ClanCommands(ZenixMCInterface zenix) {
-		super(zenix);
+	public ZenixUserInterface zui;
+	private String name;
+	private String[] desc;
+
+	public Clan(ZenixUserInterface zui, String name) {
+		this.zui = zui;
+		this.name = name;
+
+		if (used(name)) {
+			return;
+		}
+
+		create(zui, name);
 	}
+
+	// Organization methods
 
 	@Override
 	public String getName() {
-		return "clan";
+		return name;
 	}
 
 	@Override
-	public String[] getAliases() {
-		return new String[] { "c" };
+	public String[] getDescription() {
+		return desc;
 	}
 
 	@Override
-	public String[] getHelp() {
-		return null;
-	}
-
-	@Override
-	public String getFormat() {
-		return null;
-	}
-
-	@Override
-	public String getDescription() {
-		return null;
-	}
-
-	@Override
-	public boolean onCommand(ZenixCommandSender sender, String label,
-			String[] args) {
-		if (args.length == 0) {
-
+	public ZenixUserInterface[] getMembers(String name) {
+		if (!Clans.contains(name)) {
+			return null;
+		} else if (name == null) {
+			return null;
 		}
-		if (args[0].equalsIgnoreCase("create")) {
-			if (!sender.zui.isAuthorised("clans.create")) {
-				new Message(sender.zui, "No Permissions!", Type.ERROR);
-				return false;
-			} else if (args.length == 1) {
-				new Message(sender.zui, "Not enough Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan create (name)!", Type.USAGE);
-				return false;
-			} else if (args.length > 2) {
-				new Message(sender.zui, "Too many Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan create (name)!", Type.USAGE);
-				return false;
+		return getmembers(name);
+	}
+
+	public void create(ZenixUserInterface player, String name) {
+		Clans.add(name);
+		playerClan.put(player, name);
+	}
+
+	public static void disband(ZenixUserInterface player) {
+		String name = getClan(player);
+		for (ZenixUserInterface z : members(name)) {
+			playerClan.remove(z);
+		}
+		Clans.remove(name);
+		new Message(player, "You have disbanded the Clan: " + name + "!",
+				Type.CLAN);
+	}
+
+	// Methods
+
+	public static void invite(ZenixUserInterface target, String name) {
+		if (target != null) {
+			if (clanInvites.get(target).contains(name)) {
+				for (ZenixUserInterface z : members(name)) {
+					new Message(z, "The invite of " + target
+							+ " has been revoked!", Type.CLAN);
+					clanInvites.remove(target);
+					return;
+				}
+			} else if (playerClan.get(target) == name) {
+				for (ZenixUserInterface z : members(name)) {
+					new Message(z, target + " is already apart of the Clan!",
+							Type.CLAN);
+					return;
+				}
 			} else {
-				new Clan(sender.zui, args[1]);
+				clanInvites.put(target, name);
+				for (ZenixUserInterface z : members(name)) {
+					new Message(z, target + " has been invited to the Clan!",
+							Type.CLAN);
+					return;
+				}
 			}
-		} else if (args[0].equalsIgnoreCase("join")) {
-			if (!sender.zui.isAuthorised("clans.join")) {
-				new Message(sender.zui, "No Permissions!", Type.ERROR);
-				return false;
-			} else if (args.length == 1) {
-				new Message(sender.zui, "Not enough Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan join (name)!", Type.USAGE);
-				return false;
-			} else if (args.length > 2) {
-				new Message(sender.zui, "Too many Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan join (name)!", Type.USAGE);
-				return false;
-			} else {
-				Clan.join(sender.zui, Clan.getClan(sender.zui));
+		}
+	}
+
+	public static void join(ZenixUserInterface player, String name) {
+		if (playerClan.containsKey(player)) {
+			new Message(player, "You are already in a Clan!", Type.CLAN);
+			return;
+		} else {
+			playerClan.put(player, name);
+			new Message(player, "You have joined " + name + "!", Type.CLAN);
+			return;
+		}
+	}
+
+	public static void leave(ZenixUserInterface player) {
+		String name = getClan(player);
+		if (playerClan.get(player) == null) {
+			new Message(player, "You are not in a Clan!", Type.CLAN);
+			return;
+		} else {
+			for (ZenixUserInterface z : members(name)) {
+				new Message(z, player.getName() + " has left the Clan!",
+						Type.CLAN);
 			}
-		} else if (args[0].equalsIgnoreCase("leave")) {
-			if (!sender.zui.isAuthorised("clans.leave")) {
-				new Message(sender.zui, "No Permissions!", Type.ERROR);
-				return false;
-			} else if (args.length > 1) {
-				new Message(sender.zui, "Too many Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan leave!", Type.USAGE);
-				return false;
-			} else {
-				Clan.leave(sender.zui);
+			new Message(player, "You have left " + name, Type.CLAN);
+			playerClan.remove(player);
+			return;
+		}
+	}
+
+	public static void kick(ZenixUserInterface target) {
+		String name = getClan(target);
+		if (!playerClan.containsKey(target) || playerClan.get(target) != name) {
+			for (ZenixUserInterface z : members(name)) {
+				new Message(z,
+						target.getName() + " is not apart of your Clan!",
+						Type.CLAN);
+				return;
 			}
-		} else if (args[0].equalsIgnoreCase("disband")) {
-			if (!sender.zui.isAuthorised("clans.disband")) {
-				new Message(sender.zui, "No Permissions!", Type.ERROR);
-				return false;
-			} else if (args.length > 1) {
-				new Message(sender.zui, "Too many Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan disband!", Type.USAGE);
-				return false;
-			} else {
-				Clan.disband(sender.zui);
+		} else {
+			playerClan.remove(target);
+			for (ZenixUserInterface z : members(name)) {
+				new Message(z, target.getName()
+						+ " has been kicked from the Clan!", Type.CLAN);
+				return;
 			}
-		} else if (args[0].equalsIgnoreCase("kick")) {
-			if (!sender.zui.isAuthorised("clans.kick")) {
-				new Message(sender.zui, "No Permissions!", Type.ERROR);
-				return false;
-			} else if (args.length == 1) {
-				new Message(sender.zui, "Not enough Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan kick (name)!", Type.USAGE);
-				return false;
-			} else if (args.length > 2) {
-				new Message(sender.zui, "Too many Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan kick (name)!", Type.USAGE);
-				return false;
-			} else {
-				Clan.kick(sender.zui, args[1]);
+		}
+	}
+
+	public static void sethome(ZenixUserInterface player) {
+		String name = getClan(player);
+		clanHomes.put(player.getLocation(), name);
+		for (ZenixUserInterface z : members(name)) {
+			new Message(z, player.getName()
+					+ " has set a new home for your Clan!", Type.CLAN);
+		}
+
+	}
+
+	public static void gohome(ZenixUserInterface player) {
+		// TODO: Teleport player to home
+
+	}
+
+	// Getting methods
+
+	public static void invites(ZenixUserInterface player) {
+		List<String> temp = new ArrayList<>();
+		for (ZenixUserInterface z : clanInvites.keySet()) {
+			if (z == player) {
+				temp.add(clanInvites.get(z));
 			}
-		} else if (args[0].equalsIgnoreCase("invites")) {
-			if (!sender.zui.isAuthorised("clans.invites")) {
-				new Message(sender.zui, "No Permissions!", Type.ERROR);
-				return false;
-			} else if (args.length > 1) {
-				new Message(sender.zui, "Too many Arguments!", Type.ERROR);
-				new Message(sender.zui, "/clan invites!", Type.USAGE);
-				return false;
-			} else {
-				Clan.invites(sender.zui);
+		}
+		clanInvites.remove(player);
+		for (int i = 0; temp.size() > i; i++) {
+			new Message(player, temp.remove(0).toString(), Type.CLAN);
+
+			if (temp.size() == 0) {
+				break;
 			}
+		}
+	}
+
+	public static ZenixUserInterface[] members(String name) {
+		for (ZenixUserInterface z : playerClan.keySet()) {
+			if (playerClan.get(z) == name) {
+				return new ZenixUserInterface[] { z };
+			}
+		}
+		return null;
+	}
+
+	public ZenixUserInterface[] getmembers(String name) {
+		for (ZenixUserInterface z : playerClan.keySet()) {
+			if (playerClan.get(z) == name) {
+				return new ZenixUserInterface[] { z };
+			}
+		}
+		return null;
+	}
+
+	public static boolean used(String name) {
+		if (Clans.contains(name)) {
+			return true;
 		}
 		return false;
 	}
 
-	@Override
-	public List<String> onTabComplete(ZenixCommandSender sender, String alias,
-			String[] args) {
+	public static String getClan(ZenixUserInterface player) {
+		if (playerClan.get(player) != null) {
+			return playerClan.get(player);
+		}
 		return null;
 	}
-
 }
