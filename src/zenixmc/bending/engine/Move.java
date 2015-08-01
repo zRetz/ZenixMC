35831c56f406d0d43d7a1597caed5c7593da2463
grasshopperMatt <grasshoppermatt3@gmail.com>
2015-08-01 13:22:41 -0400

    Convenience push

---
+++ src/zenixmc/bending/engine/Move.java
@@ -0,0 +1,38 @@
+package zenixmc.bending.engine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Move implements Engine {
+	public List<State> states = new ArrayList<>();
+	public String name;
+	public String[] desc;
+	
+	public Move(String name, String[] desc) {
+		this.name = name;
+		this.desc = desc;
+		
+		states.add(new State());
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String[] getHelp() {
+		return desc;
+	}
+
+	@Override
+	public double getSpeed() {
+		return 0;
+	}
+
+	@Override
+	public State getStates() {
+		return null;
+	}
+
+}
