313777596d8595c6546ee42907e3039b1c08da60
Matt <grasshopperMatt@users.noreply.github.com>
2015-07-31 22:15:26 -0400

    Added OrganizationPlayerData.java

---
+++ src/zenixmc/organization/OrganizationPlayerData.java
@@ -0,0 +1,25 @@
+package zenixmc.organization;
+
+import java.util.Map;
+
+public class OrganizationPlayerData {
+
+	/**
+	 * The maximum amount of influence.
+	 */
+	public int maxInfluence;
+	
+	/**
+	 * The amount of influence.
+	 */
+	public int influence;
+	
+	/**
+	 * The organizations in map form.
+	 */
+	public Map<String, String> organizations;
+	public Map<OrganizationPlayerInterface, Organization> invites = Organization.invites;
+	
+	public OrganizationPlayerData() {	
+	}
+}
